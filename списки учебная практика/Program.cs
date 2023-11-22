fa2ba712e9bd1add5f9787199b26518d45a4cee3
maxim <maks.shipilow@mail.ru>
2023-11-22 22:57:21 +0700

    clennnnnnnn

---
+++ списки учебная практика/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace списки
+{
+	class airport
+	{
+		
+		public void vvod()
+		{
+			var someList = new List<string>();
+			{ 
+			};
+			Console.WriteLine("введите количество самолетов");
+			int n;
+			int j = 1;
+			n = Convert.ToInt32(Console.ReadLine());
+			string[] s = new string[n];
+			for (int k = 0; k < n; k++)
+			{
+				Console.WriteLine("введите название фирмы самолета");
+				s[k] = Console.ReadLine();
+				someList.Add(s[k]);
+			
+			}
+			
+			for (int k = 0; k < n; k++)
+			{
+				Console.WriteLine(j + " самолет: " + s[k]);
+				j++;
+			
+			}
+			foreach (var L in someList)
+			{
+				Console.WriteLine(L);
+			}
+			Console.WriteLine();
+		}
+		public void vivod()
+		{ 
+		}
+
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			airport A = new airport();
+			A.vvod();
+			
+		}
+	}
+}
