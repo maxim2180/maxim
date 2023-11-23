432cc86314b6128d2776269d38d9fac086a3798e
TheDebikSlayer <romashov6699@gmail.com>
2023-11-23 10:50:48 +0700

    fdf

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string[] a = new string[5];
+            int i;
+            for ( i = 0; i < 5; i++)
+                Console.WriteLine(" введите букву");
+            a[i] = Convert.ToString(Console.ReadLine());*/
+            int i = -1;
+            while (i < 0)
+                Console.Write(" тема пиддор");
+        }
+    }
+}
